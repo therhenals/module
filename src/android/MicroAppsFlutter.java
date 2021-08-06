c9c900e5b412783613711f45cfc938133535ac3f
Luis Manuel Rhenals Castro <jrhenals@todo1.net>
2021-08-06 11:49:48 -0500

    Added Copyright

--- src/android/MicroAppsFlutter.java
+++ src/android/MicroAppsFlutter.java
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) TODO1 SERVICES, INC. (‘TODO1’) All rights reserved, 2000 – 2021. This work is protected by the United States of America copyright laws. All information contained herein is and remains the property of TODO1 [and its suppliers, if any] *. 
+    Dissemination of this information or reproduction of this material is not permitted unless prior written consent is obtained from TODO1 SERVICES, INC.
+*/
+
 package com.todo1.plugins.MicroAppsFlutter;
 
 import org.apache.cordova.CordovaPlugin;
